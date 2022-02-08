9009d18ef6bced97abb417576a9f0723501390bc
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-08 17:28:25 -0500

    halfway through writing my endpoints but tests arent working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,84 @@
 // implement your posts router here
+
 const express = require('express')
 
 const router = express.Router()
 
 module.exports = router
+
+// data access functions
+const Post = require('./posts-model')
+
+// THIS IS WHERE WE MAKE ENDPOINTS
+// use option + command + up arrow to write multiple lines at once
+
+
+router.get('/', (req, res) => {
+    Post.find()
+    .then(found => {
+        //"throw new Error('ouch')" to check that error code is working like a console.log()
+        res.json(found)
+    }).catch(err => {
+        res.status(500).json({
+            message: '',
+            err: 'The posts information could not be retrieved',
+            stack: err.stack
+        })
+    })
+})
+router.get('/:id', async (req, res) => {
+    try {
+        //throw new Error('whyyyy')
+        const post = await Post.findById(req.params.id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            // DONT FORGET ABOUT THE ELSE -- either have to do else or a return inside the if statement
+            res.json(post)
+        }
+        
+    } catch (err) {
+        res.status(500).json({
+            message: 'The post with this information does not exist',
+            err: err.message,
+            stack: err.stack
+        })
+    }
+
+})
+router.post('/', (req, res) => {
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message:'Please provide title and contents for the post'
+        })
+    } else {
+        // client supplied correct info
+        Post.insert({ title, contents })
+            .then(({id}) => {
+             // stuff comes back as just an object with an id, so destructure id in order to get everything back
+            return Post.findById(id)
+            .then(post => {
+                res.status(201).json(post)
+            })
+        }).catch(err => {
+            res.status(500).json({
+                message: 'There was an error while saving the post to the database',
+                err: err.message,
+                stack: err.stack
+            })
+        })
+    }
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
--- api/server.js
+++ api/server.js
@@ -3,9 +3,9 @@
 
 const express = require('express');
 const postsRouter = require('./posts/posts-router')
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 server.use('api/posts', postsRouter)
 
